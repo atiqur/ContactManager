f7264a90c9c8d25803a1129b515fd13ceecbc0d4
Atiqur Rahman <atiqur.india@gmail.com>
2021-08-12 09:47:34 +0530

    CRUD - Add and Get Contact Methods

--- app/src/main/java/com/example/contactmanager/data/DatabaseHandler.java
+++ app/src/main/java/com/example/contactmanager/data/DatabaseHandler.java
@@ -1,23 +1,24 @@
 package com.example.contactmanager.data;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import androidx.annotation.Nullable;
-
 import com.example.contactmanager.R;
+import com.example.contactmanager.model.Contact;
 import com.example.contactmanager.util.Util;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
 
-    public DatabaseHandler(@Nullable Context context) {
+    public DatabaseHandler(Context context) {
         super(context, Util.DATABASE_NAME, null, Util.DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String CREATE_CONTACT_TABLE = "CREATE TABLE " + Util.TABLE_NAME + "("
+        String CREATE_CONTACT_TABLE = "CREATE TABLE " + Util.TABLE_NAME  + "("
                 + Util.KEY_ID + " INTEGER PRIMARY KEY, "
                 + Util.KEY_NAME + " TEXT, "
                 + Util.KEY_PHONE_NUMBER + " TEXT" + ")";
@@ -27,9 +28,38 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         String DROP_TABLE = String.valueOf(R.string.drop_table);
-        db.execSQL(DROP_TABLE, new String[]{Util.DATABASE_NAME});
+        db.execSQL(DROP_TABLE, new String[]{Util.TABLE_NAME});
 
-        // Create a table again
         onCreate(db);
+    }
+
+    public void addContact(Contact contact) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put(Util.KEY_NAME, contact.getName());
+        values.put(Util.KEY_PHONE_NUMBER, contact.getPhoneNumber());
+
+        db.insert(Util.TABLE_NAME, null, values);
+        db.close();
+    }
+
+    public Contact getContact(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(Util.TABLE_NAME,
+                new String[]{Util.KEY_ID, Util.KEY_NAME, Util.KEY_PHONE_NUMBER},
+                Util.KEY_ID + "=?", new String[]{String.valueOf(id)},
+                null, null, null);
+
+        if (cursor != null)
+            cursor.moveToFirst();
+
+        Contact contact = new Contact();
+        contact.setId(Integer.parseInt(cursor.getString(0)));
+        contact.setName(cursor.getString(1));
+        contact.setPhoneNumber(cursor.getString(2));
+
+        return contact;
     }
 }
